c4ed05b0bcd23b91b481dd8a53942b40bf126259
GiftLanga <glanga@retrorabbit.co.za>
2019-06-27 16:15:55 +0200

    homepage setupe

--- chatApp/author/urls.py
+++ chatApp/author/urls.py
@@ -2,5 +2,5 @@ from author import views
 from django.urls import path
 
 urlpatterns = [
-  path('login/', views.index)
+  path('login/', views.authenticate)
 ]
--- chatApp/author/views.py
+++ chatApp/author/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
+def authenticate(request):
   my_dict = {'component':'<h1> THe component </h1>'}
   return render(request, 'index.html', context=my_dict)
